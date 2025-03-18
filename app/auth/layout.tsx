c6eed351cef06d7e7504096e9b4e288eda4ef603
Juan Fer <juan.narvaez@greenrun.com>
2025-03-17 20:53:24 -0500

    added link to home in logo

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/ui/Logo";
 import ToasNotification from "@/components/ui/ToastNotification";
+import Link from "next/link";
 
 export default async function AuthLayout({
   children,
@@ -11,7 +12,9 @@ export default async function AuthLayout({
       <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
         <div className="flex justify-center lg:bg-[url(/grafico.svg)] lg:bg-[length:-30rem] bg-no-repeat bg-left-bottom bg-purple-950">
           <div className="w-96 py-10 lg:py-20">
-            <Logo />
+            <Link href="/">
+              <Logo />
+            </Link>
           </div>
         </div>
         <div className="p-10 lg:py-28">
